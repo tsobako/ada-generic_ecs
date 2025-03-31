8f8aaf283b155a7a1ade336b6b570aed067ce692
Ihar Tsalka <git@tsobako.me>
2025-03-31 11:27:05 +0300

    Update demo with resources

--- demo/src/demonstration.adb
+++ demo/src/demonstration.adb
@@ -1,26 +1,48 @@
 with Ada.Text_IO;
+with Ada.Calendar;
 with Generic_ECS;
 
 procedure Demonstration is
    type Component_Kind_Type is (Position_Kind);
-   package ECS is new Generic_ECS (Component_Kind_Type);
+   type Resource_Kind_Type is (Time_Kind);
+   --  if you are not planning to use any resource, declare resource kind as empty range
+   --  type Resource_Kind_Type is range 1 .. 0;
+   --  But adding/updating/deleting any resource will cause Constraint_Error;
+   package ECS is new Generic_ECS (Component_Kind_Type, Resource_Kind_Type);
 
    type Position_Type is new ECS.Component_Interface_Type with record
       X, Y, Z : Float;
    end record;
    type Position_Access_Type is access all Position_Type;
 
+
+   type Time_Type is new ECS.Resource_Interface_Type with record
+      Seconds: Natural;
+   end record;
+   type Time_Access_Type is access all Time_Type;
+
    procedure System
      (Registry : ECS.Registry_Type;
       Entity   : ECS.Entity_Type)
    is
       Position : constant Position_Access_Type := Position_Access_Type (Registry.Get (Entity, Position_Kind));
+      Current_Time : constant Time_Access_Type := Time_Access_Type (Registry.Get_Resource(Time_Kind));
    begin
-      Ada.Text_IO.Put_Line ("X:" & Position.X'Img);
+      Ada.Text_IO.Put_Line ("X at time" & Current_Time.Seconds'Image & " :" & Position.X'Image);
    end System;
 
    Registry : ECS.Registry_Type := ECS.Initialize;
 begin
+
+   -- Add time resource
+   declare
+      Current_Time : constant Ada.Calendar.Time := Ada.Calendar.Clock;
+      Time : constant Time_Access_Type := new Time_Type;
+   begin
+      Time.Seconds := Natural(Ada.Calendar.Seconds(Current_Time));
+      Registry.Add_Resource(Time_Kind, ECS.Resource_Interface_Class_Access_Type(Time));
+   end;
+
    -- Create 5 entities
    for Index in 1 .. 5 loop
       declare
